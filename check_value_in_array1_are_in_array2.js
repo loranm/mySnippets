bd7d14955cebb0c40e31df2e2b2c44b53498d796
loran <chmoxster@gmail.com>
2016-12-01 06:38:11 +0100

    Correction d'erreur

--- check_value_in_array1_are_in_array2.js
+++ check_value_in_array1_are_in_array2.js
@@ -1,33 +1,30 @@
 var arr1 = [{
-    prenom: 'lison'
+  prenom: 'lison'
 }, {
-    prenom: 'suzette'
+  prenom: 'suzette'
 }, {
-    prenom: 'laurent'
-}, {
-    prenom: 'yann'
-}];
+  prenom: 'laurent'
+},{prenom:"hubert"}];
 
 var arr2 = [{
-    prenom: 'lison'
-}, {
-    prenom: 'suzette'
-}, {
-    prenom: 'laurent'
-}];
+  prenom: 'lison'
+},  {
+  prenom: 'laurent'
+},{prenom:"hubert"}];
+
 
 function check2Arrays(a, b) {
     for (var i = 0; i < a.length; i++) {
         for (var j = 0; j < b.length; j++) {
-            if (a.length > 0) {
+            if (a.length) {
                 if (a[i].prenom === b[j].prenom) {
                     a.splice(i, 1);
-                } else {
-                    return a;
                 }
+              } else {
+                    return a;
+              }
             }
         }
-    }
 
     return a;
 }
